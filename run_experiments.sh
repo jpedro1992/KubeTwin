b29ec51994c2db229929db763c7c7f0c17639491
Jose Santos <josepedro.pereiradossantos@ugent.be>
2025-10-07 15:48:50 +0200

    further experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,71 +1,35 @@
 #!/bin/bash
 
 # Define testing variables here
-TEST="test_img_rec_v3.conf"
-TEST_DIR="experiments/test_img/hpa_v3"
+TEST="test_img_rec_random_bursts.conf"
+BASE_DIR="experiments/test_img/hpa_random_bursts"
 
-# bundle exec bin/kube_twin <other_config.conf> > <other_output.txt>
-STRATEGY="BALANCED_WITH_TOPOLOGY"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/balanced_with_topology.txt"
+STRATEGIES=(
+  "BALANCED_WITH_TOPOLOGY"
+  "BALANCED"
+  "DIKTYO"
+  "NODE_AFFINITY"
+  "TRIMARAN_LOW_RISK"
+  "TOPOLOGY_AWARE"
+  "LEAST_ALLOCATABLE"
+  "MOST_ALLOCATABLE"
+  "RESOURCE_AVAILABILITY"
+  "COST_AWARE"
+  "DIKTYO_COST"
+  "DIKTYO_RISK"
+  "DIKTYO_TOPOLOGY"
+)
 
-STRATEGY="BALANCED"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/balanced.txt"
+# Number of repetitions
+REPEAT=5
 
-STRATEGY="DIKTYO"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/diktyo.txt"
-
-STRATEGY="NODE_AFFINITY"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/node_affinity.txt"
-
-STRATEGY="TRIMARAN_LOW_RISK"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/trimaran.txt"
-
-STRATEGY="TOPOLOGY_AWARE"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/topology.txt"
-
-STRATEGY="LEAST_ALLOCATABLE"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/least_allocatable.txt"
-
-STRATEGY="MOST_ALLOCATABLE"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/most_allocatable.txt"
-
-STRATEGY="RESOURCE_AVAILABILITY"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/resource_availability.txt"
-
-STRATEGY="COST_AWARE"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/cost.txt"
-
-STRATEGY="DIKTYO_COST"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/diktyo_cost.txt"
-
-STRATEGY="DIKTYO_RISK"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/diktyo_risk.txt"
-
-STRATEGY="DIKTYO_TOPOLOGY"
-sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
-echo "--------------Running experiment with scheduling strategy: $STRATEGY----------------------"
-bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/diktyo_topology.txt"
+for ((i=1; i<=REPEAT; i++)); do
+    TEST_DIR="$BASE_DIR/run_$i"
+    mkdir -p "$TEST_DIR"
+    echo "-------------- Starting Run $i----------------------"
+    for STRATEGY in "${STRATEGIES[@]}"; do
+        sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
+        echo "-------------- Running experiment with scheduling strategy: $STRATEGY ----------------------"
+        bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/${STRATEGY,,}.txt"
+    done
+done
