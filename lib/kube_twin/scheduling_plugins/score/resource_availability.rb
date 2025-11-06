51404e1953f37615e53552d538ddb74a2fa432ea
Jose Santos <josepedro.pereiradossantos@ugent.be>
2025-11-06 14:19:46 +0100

    different weights in resource availability

--- lib/kube_twin/scheduling_plugins/score/resource_availability.rb
+++ lib/kube_twin/scheduling_plugins/score/resource_availability.rb
@@ -1,7 +1,9 @@
 module KUBETWIN
   class ResourceAvailabilityScore
     def self.run(filtered_nodes, _nodes, _node_affinity)
-      puts "[Scheduler] Starting ResourceAvailabilityScore plugin...'"
+      w_cpu = 0.75
+      w_mem = 0.25
+      puts "[Scheduler] Starting ResourceAvailabilityScore plugin... w_cpu #{w_cpu} and w_mem #{w_mem}'"
       # Score nodes by available CPU and Memory combined (weighted average)
       max_cpu = filtered_nodes.map { |n| n[:node].available_resources_cpu }.max.to_f
       max_mem = filtered_nodes.map { |n| n[:node].available_resources_memory }.max.to_f
@@ -16,8 +18,7 @@ module KUBETWIN
         mem_score = (entry[:node].available_resources_memory.to_f / max_mem) * 100
 
         # weighted average (equal weight)
-        combined_score = (cpu_score + mem_score) / 2.0
-
+        combined_score = (w_cpu * cpu_score + w_mem * mem_score) / 2.0
         # puts "[Scheduler] Node #{entry[:node].node_id} score: #{combined_score.round(2)}"
 
         { node: entry[:node], score: combined_score }
