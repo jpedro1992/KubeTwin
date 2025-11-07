22de1c40f20884d221fd3456ed59d0fcd45a3c80
Jose Santos <josepedro.pereiradossantos@ugent.be>
2025-11-07 15:22:30 +0100

    updated script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define testing variables here
-TEST="cuttlefish_ramp_up_15min.conf"
-BASE_DIR="experiments/cuttlefish/ramp_up_15min_v2"
+TEST="fc_img_rec_random_bursts_15min.conf"
+BASE_DIR="experiments/fog-cloud/img_rec/random_bursts_15min"
 
 STRATEGIES=(
   "BALANCED_WITH_TOPOLOGY"
@@ -21,15 +21,38 @@ STRATEGIES=(
 )
 
 # Number of repetitions
-REPEAT=5
+REPEAT=10
 
 for ((i=1; i<=REPEAT; i++)); do
+    if [ ! -d "$BASE_DIR" ]; then
+        echo "Creating base directory: $BASE_DIR"
+        mkdir -p "$BASE_DIR"
+    fi
+
     TEST_DIR="$BASE_DIR/run_$i"
-    mkdir -p "$TEST_DIR"
+    if [ ! -d "$TEST_DIR" ]; then
+        echo "Creating test directory: $TEST_DIR"
+        mkdir -p "$TEST_DIR"
+    fi
+
+    export KUBETWIN_SEED=$RANDOM
+
     echo "-------------- Starting Run $i----------------------"
+    echo "-------------- Using Seed $KUBETWIN_SEED ----------------------"
     for STRATEGY in "${STRATEGIES[@]}"; do
+        STRATEGY_LOWER_CASE=$(echo "$STRATEGY" | tr '[:upper:]' '[:lower:]')
+
+        # Update the strategy in the config file
         sed -i "s/^strategy .*/strategy :$STRATEGY/" examples/$TEST
+
+        # Update the seed in the config file
+        sed -i "s/seed 12345/seed $KUBETWIN_SEED/" examples/$TEST
+
+        OUT_FILE="$TEST_DIR/$STRATEGY_LOWER_CASE.txt"
+        # ensure file exists before redirect
+        # touch "$OUT_FILE"
+
         echo "-------------- Running experiment with scheduling strategy: $STRATEGY ----------------------"
-        bundle exec bin/kube_twin "examples/$TEST" > "$TEST_DIR/${STRATEGY,,}.txt"
+        bundle exec bin/kube_twin "examples/$TEST" > "$OUT_FILE"
     done
 done
